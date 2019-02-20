fe5f346e3009ab30a3569c872232728a175f4190
joao <joao182cx@gmail.com>
2019-02-20 11:21:03 -0300

    Criação do código que imprime no cheque o valor passado pelo usuário.

--- src/Cheque.java
+++ src/Cheque.java
@@ -6,10 +6,19 @@ public class Cheque {
 	public static void main(String[] args) {
 		
 		String valor;
+		int digitos = 0;
 		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Insira o valor que será colocado no cheque no formato 1,234.60");
 		valor = input.nextLine();
+		
+		System.out.printf("%n%nImpressão do cheque%n%n");
+		
+		for(int i = 0; i < 9 - valor.length(); i++) {
+			
+			System.out.print("*");
+			
+		}
 		
 		System.out.println(valor);
 		
