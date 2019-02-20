ab6b0c25e1fe18f12842787ebfc2bc5831e92eab
joao <joao182cx@gmail.com>
2019-02-20 11:08:35 -0300

    Leitura do valor que será impresso no cheque.

---
+++ src/Cheque.java
@@ -0,0 +1,20 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Cheque {
+
+	public static void main(String[] args) {
+		
+		String valor;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Insira o valor que será colocado no cheque no formato 1,234.60");
+		valor = input.nextLine();
+		
+		System.out.println(valor);
+		
+		System.out.printf("---------%n123456789");
+
+	}
+
+}
